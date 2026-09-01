4764329ea3b45a2c6f5ec7582c2f266dec6f238e
treepolo <bigtreepolo@gmail.com>
2026-09-01 14:48:21 +0800

    feat: inject initial report spotlight

--- src/export/report-renderer.js
+++ src/export/report-renderer.js
@@ -8,6 +8,7 @@ const { injectXp7RangeTheme } = require('./xp7-range-theme');
 const { injectReportHelpHtml } = require('./report-help-runtime');
 const { injectReportLayoutRefinement } = require('./report-layout-refinement');
 const { injectReportFloatingUiRefinement } = require('./report-floating-ui-refinement');
+const { injectReportEntrySpotlight } = require('./report-entry-spotlight');
 
 function renderReportHtml(reportDocument, options = {}) {
   const portable = base.toPortableReportDocument(reportDocument);
@@ -19,6 +20,7 @@ function renderReportHtml(reportDocument, options = {}) {
   html = injectReportHelpHtml(html);
   html = injectReportLayoutRefinement(html);
   html = injectReportFloatingUiRefinement(html);
+  html = injectReportEntrySpotlight(html);
   return html;
 }
 
